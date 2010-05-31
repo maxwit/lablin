511c9d1afadad6dd731bba1a6a673345a2bfb979
Conke Hu <conke@maxwit.com>
2010-05-31 21:47:57 +0800

    add DirectFB

--- application/DirectFB-1.2.8/build.sh
+++ application/DirectFB-1.2.8/build.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 #
-# http://maxwit.googlecode.com
+# http://code.google.com/p/maxwit/
 #
 
-ac_cv_header_asm_page_h=no \
+#ac_cv_header_asm_page_h=no \
+#hardcode_into_libs=no \
+
+TSLIB_CFLAGS=${SYSROOT_PATH}/usr/include \
+TSLIB_LIBS=${SYSROOT_PATH}/usr/lib \
 ./configure \
 	--prefix=/usr \
-	--sysconfdir=/etc \
-	--with-gfxdrivers=none \
-	--with-inputdrivers=keyboard,linuxinput,ps2mouse,tslib \
-	--enable-fbdev \
-	--disable-sdl \
-	|| exit 1
+	--build=${BUILD_PLAT} \
+	--host=${TARGET_PLAT} \
+    --with-gfxdrivers=none \
+    --disable-x11 \
+    --disable-sdl \
+    --enable-fbdev \
+    --enable-zlib \
+    --with-inputdrivers=keyboard,linuxinput,ps2mouse,tslib \
+    --disable-video4linux \
+    --disable-video4linux2 || exit 1
 
-make && make install || exit 1
+make || exit
+make DESTDIR=${SYSROOT_PATH} install || exit
+
+#sed -i "s:^prefix=/usr$:prefix=${SYSROOT_PATH}/usr:" ${CROSS_PC_PATH}/direct*.pc && \
+#sed -i "s:^prefix=/usr$:prefix=${SYSROOT_PATH}/usr:" ${CROSS_PC_PATH}/fusion*.pc || exit 1
 
--- application/DirectFB-examples-1.2.0/build.sh
+++ application/DirectFB-examples-1.2.0/build.sh
@@ -2,8 +2,12 @@
 #
 #
 
+ac_cv_path_PKG_CONFIG=pkg-config 
+./configure \
+	--prefix=${SYSROOT_PATH}/usr \
+	--build=${BUILD_PLAT} \
+	--host=${TARGET_PLAT} \
+	|| exit 1
 
-ac_cv_path_PKG_CONFIG=pkg-config ./configure --prefix=/usr || exit 1
-
-make && make prefix=/usr install || exit 1
+make && make DESTDIR=${SYSROOT_PATH} prefix=/usr install || exit 1
 
