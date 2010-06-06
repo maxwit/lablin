f9edb89b3a9038569efabc37c621fe563bb12e19
Conke Hu <conke@maxwit.com>
2010-06-06 14:13:29 +0800

    Directfb examples: fix include path
    Signed-off-by: Linke Deng <linke@maxwit.com>
    Signed-off-by: Spark Zhou <spark.tribbiani@gmail.com>

--- application/DirectFB-examples-1.2.0/build.sh
+++ application/DirectFB-examples-1.2.0/build.sh
@@ -2,15 +2,12 @@
 #
 #
 
-
-# ac_cv_path_PKG_CONFIG=pkg-config
-
-DIRECTFB_CFLAGS="-I/home/conke/maxwit/sysroot/usr/include/directfb" \
+DIRECTFB_CFLAGS="-I${ROOTFS_PATH}/usr/include/directfb" \
 ./configure \
 	--prefix=/usr \
 	--build=${BUILD_PLAT} \
 	--host=${TARGET_PLAT} \
 	|| exit 1
 
-make && make DESTDIR=${ROOTFS_PATH} prefix=/usr install || exit 1
-
+make && \
+make DESTDIR=${ROOTFS_PATH} prefix=/usr install || exit 1
