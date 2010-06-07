e074f58f6108d8edf8d865a82f70aeff246615f4
Conke Hu <conke@maxwit.com>
2010-06-07 18:55:01 +0800

    fix libpng
    Signed-off-by: Conke Hu <conke@maxwit.com>

--- application/libpng-1.4.2/build.sh
+++ application/libpng-1.4.2/build.sh
@@ -9,9 +9,8 @@
 	--host=${TARGET_PLAT} \
 	--enable-shared \
 	--enable-static \
-	--with-libpng-compat \
 	|| exit 1
 
 make || exit 1
 make DESTDIR=${ROOTFS_PATH} install || exit 1
-sed -i "s:^libdir=.*:libdir=${ROOTFS_PATH}/usr/lib:" ${ROOTFS_PATH}/usr/lib/libpng12.la
+sed -i "s:^libdir=.*:libdir=${ROOTFS_PATH}/usr/lib:" ${ROOTFS_PATH}/usr/lib/libpng14.la
