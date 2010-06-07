9a3b786777e9c05109a731603559f00e8d9773af
Conke Hu <conke@maxwit.com>
2010-06-08 00:00:01 +0800

    fix prboom for compatibility with libpng14
    Signed-off-by: Conke Hu <conke@maxwit.com>

--- application/prboom-2.5.0/build.sh
+++ application/prboom-2.5.0/build.sh
@@ -13,6 +13,6 @@
 	|| exit 1
 
 sed -i -e "/\#define gid_t int/d" -e "/\#define uid_t int/d" config.h
+sed -i "s/png_[a-z_]*_NULL/NULL/g" src/SDL/i_sshot.c
 
 make && make DESTDIR=${ROOTFS_PATH} install || exit 1
-
