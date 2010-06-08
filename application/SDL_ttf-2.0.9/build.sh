533712741e1ba65665e7ec412fe9bd1a7b0ca0ef
Conke Hu <conke@maxwit.com>
2010-06-08 18:57:29 +0800

    fix freetype-config path for SDL_ttf configure
    Signed-off-by: Conke Hu <conke@maxwit.com>

--- application/SDL_ttf-2.0.9/build.sh
+++ application/SDL_ttf-2.0.9/build.sh
@@ -7,9 +7,10 @@
 hardcode_into_libs=no \
 ./configure \
 	--prefix=/usr \
+	--with-sdl-exec-prefix=${ROOTFS_PATH}/usr \
+	--with-freetype-prefix=${ROOTFS_PATH}/usr \
 	--build=${BUILD_PLAT} \
 	--host=${TARGET_PLAT} \
-	--with-sdl-exec-prefix=${ROOTFS_PATH}/usr \
 	--enable-sdltest  \
 	--without-x \
 	|| exit 1
