f3c3217b7acb73da53ae616800335a427d29f1a3
chuck <chuck.hcg@gmail.com>
2010-07-11 23:31:34 +0800

    wireless_tools: fix building path
    Signed-off-by: chuck <chuck.hcg@gmail.com>

--- target/wireless_tools.29/build.sh
+++ target/wireless_tools.29/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-sed -i "7s:^ifndef PREFIX:\#ifndef PREFIX:g" ${ROOTFS_PATH}/../build/application/wireless_tools.29/Makefile
-sed -i "9s:^endif:PREFIX=${ROOTFS_PATH}\/usr:g" ${ROOTFS_PATH}/../build/application/wireless_tools.29/Makefile
-sed -i "s:^CC = gcc:CC=arm-linux-gcc:g" ${ROOTFS_PATH}/../build/application/wireless_tools.29/Makefile
-sed -i "s:^AR =.*$:AR=arm-linux-ar:g" ${ROOTFS_PATH}/../build/application/wireless_tools.29/Makefile
-sed -i "s:^RANLIB =.*$:RANLIB=arm-linux-ranlib:g" ${ROOTFS_PATH}/../build/application/wireless_tools.29/Makefile
+sed -i "7s:^ifndef PREFIX:\#ifndef PREFIX:g" ${ROOTFS_PATH}/../build/target/wireless_tools.29/Makefile
+sed -i "9s:^endif:PREFIX=${ROOTFS_PATH}\/usr:g" ${ROOTFS_PATH}/../build/target/wireless_tools.29/Makefile
+sed -i "s:^CC = gcc:CC=arm-linux-gcc:g" ${ROOTFS_PATH}/../build/target/wireless_tools.29/Makefile
+sed -i "s:^AR =.*$:AR=arm-linux-ar:g" ${ROOTFS_PATH}/../build/target/wireless_tools.29/Makefile
+sed -i "s:^RANLIB =.*$:RANLIB=arm-linux-ranlib:g" ${ROOTFS_PATH}/../build/target/wireless_tools.29/Makefile
 
 make || exit
 make install || exit
