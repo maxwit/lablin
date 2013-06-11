f51fbb81419c358838e86e32f8ce9cc4d1e071eb
Conke Hu <conke.hu@maxwit.com>
2013-06-12 00:11:22 +0800

    misc fix
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- sysbuild/x86_sysbuild.sh
+++ sysbuild/x86_sysbuild.sh
@@ -6,52 +6,111 @@
 # $0 <device> [board]
 
 TOP_DIR=$PWD
-SOURCE=/maxwit/source
-BUILD=/maxwit/build
-ROOTFS="/mnt/2"
+SOURCE="/maxwit/source"
+BUILD="/maxwit/build"
+ROOTFS="/maxwit/image/rootfs"
 
 BUSYBOX="busybox-1.21.0"
 ALSA_LIB="alsa-lib-1.0.25"
 LINUX="linux-3.8.12"
 MPG123="mpg123-1.15.3"
 
-MACH=`uname -m`
-if [ $MACH = i686 ]; then
-	MACH=i386
-fi
-
-if [ $# != 1 -o ! -e "$1" ]; then
-	echo "usage: $0 <image name>/<device name>"
-	exit 1
-fi
-
-if [ -b $1 ]; then
-	ISIMG=0
-	DEV=$1
-	echo "Using physical device..."
-elif [ -f $1 ]; then
-	ISIMG=1
-	IMG=$1
-	echo "Using image file..."
-else
-	echo "invalid \"$1\"!"
-	exit 1
-fi
+#MACH=`uname -m`
+#if [ $MACH = i686 ]; then
+#	MACH=i386
+#fi
+#
+#if [ $# != 1 -o ! -e "$1" ]; then
+#	echo "usage: $0 <image name>/<device name>"
+#	exit 1
+#fi
+#
+#if [ -b $1 ]; then
+#	ISIMG=0
+#	DEV=$1
+#	echo "Using physical device..."
+#elif [ -f $1 ]; then
+#	ISIMG=1
+#	IMG=$1
+#	echo "Using image file..."
+#else
+#	echo "invalid \"$1\"!"
+#	exit 1
+#fi
+#
+#mkdir -vp ${ROOTFS}/{usr,lib,boot,proc,sys,dev,etc,var,tmp,root}
+#
+#echo "building $LINUX ..."
+#if [ ! -f "${ROOTFS}/boot/vmlinuz" ]; then
+#	cp -v /boot/vmlinuz-`uname -r` ${ROOTFS}/boot/vmlinuz && \
+#	cp -v /boot/initrd.img-`uname -r` ${ROOTFS}/boot/initrd.img && \
+#	cp -av --parents /lib/modules/`uname -r` $ROOTFS || exit 1
+#else
+#	cd $BUILD
+#	tar xf $SOURCE/$LINUX.tar.xz && \
+#	cd $LINUX && \
+#	cp -v $TOP_DIR/qemu_defconfig arch/x86/configs/ && \
+#	make qemu_defconfig && \
+#	make -j2 && \
+#	cp -v arch/x86/boot/bzImage $ROOTFS/boot/vmlinuz && \
+#	make INSTALL_MOD_PATH=$ROOTFS modules_install || exit 1
+#fi
+#
+#if [ ! -e "${ROOTFS}/lib/libc.so" ]; then
+#	cp -av --parents `find /lib /lib64 -name "*.so*"` $ROOTFS || exit 1
+#	# fixme
+#	cp -av --parents `ls /usr/lib/$MACH-linux-gnu/*w.so*` $ROOTFS || exit 1
+#	cp -av --parents `ls /usr/lib/$MACH-linux-gnu/libltdl.so*` ${ROOTFS}
+#fi
+#
+#if [ ! -e "${ROOTFS}/sbin/init" ]; then
+#	cd $BUILD
+#	tar xf $SOURCE/${BUSYBOX}.tar.bz2
+#	cd ${BUSYBOX}
+#	make defconfig > /dev/null
+#	make
+#	make CONFIG_PREFIX=$ROOTFS install
+#fi
+#
+#if [ ! -e "${ROOTFS}/etc/init.d/rcS" ]; then
+#	mkdir -p ${ROOTFS}/etc/init.d/
+#	cp -av $TOP_DIR/rcS ${ROOTFS}/etc/init.d/
+#	cp -av $TOP_DIR/group ${ROOTFS}/etc/
+#fi
+#
+#cd $BUILD
+#tar xf $SOURCE/${ALSA_LIB}.tar.bz2
+#cd ${ALSA_LIB}
+#./configure --prefix=/usr --disable-python
+#make
+#make DESTDIR=$ROOTFS install
+#
+#cp -av --parents `which aplay` $ROOTFS/
+#
+##cd $BUILD
+##tar xf $SOURCE/$MPG123.tar.bz2
+##cd $MPG123
+##./configure --prefix=/usr --with-audio=alsa && \
+##make && \
+##make DESTDIR=$ROOTFS install || exit 1
+#
+#cp -v /maxwit/document/appdevel/src/audio/mixer/mixer ${ROOTFS}/usr/bin
+#cp -v /usr/share/sounds/alsa/Front_Right.wav ${ROOTFS}
 
 if [ $ISIMG = 1 ]; then
 	fdisk $IMG < $TOP_DIR/fdisk.cmd
 
-	sudo losetup -f $IMG
+	losetup -f $IMG
 
 	OFF1SEC=`fdisk -l "$IMG" | grep "${IMG}2" | awk {'printf $2'}`
 	if [ "$OFF1SEC" = "*" ]; then
 		OFF1SEC=`fdisk -l "$IMG" | grep "${IMG}2" | awk {'printf $3'}`
 	fi
 	OFF1BYTE=$((OFF1SEC*512))
-	sudo losetup -o $OFF1BYTE -f $IMG
+	losetup -o $OFF1BYTE -f $IMG
 
-	DEV=`sudo losetup -a | sed -n '1p' | awk -F: '{printf $1}'`
-	DEV2=`sudo losetup -a | sed -n '2p' | awk -F: '{printf $1}'`
+	DEV=`losetup -a | sed -n '1p' | awk -F: '{printf $1}'`
+	DEV2=`losetup -a | sed -n '2p' | awk -F: '{printf $1}'`
 else
 	sudo fdisk $DEV < $TOP_DIR/fdisk.cmd
 
@@ -71,83 +130,25 @@ fi
 # fixme: by a partition table
 sudo mkfs.ext4 -L rootfs $DEV2
 
-sudo mkdir -vp $ROOTFS
-sudo mount $DEV2 $ROOTFS
+MNT="/mnt/2"
 
-if [ ! -d "${BOOT}/grub" ]; then
+sudo mount $DEV2 $MNT
+
+sudo cp -av $ROOTFS/* $MNT/
+
+if [ ! -d "${MNT}/boot/grub" ]; then
 	if [ $ISIMG = 1 ]; then
 		GRUB_OPT="--modules=part_msdos"
 	fi
 
-	sudo grub-install $GRUB_OPT --boot-directory=${ROOTFS}/boot $DEV
-	sudo cp -v grub.cfg ${ROOTFS}/boot/grub/
+	grub-install $GRUB_OPT --boot-directory=${MNT}/boot $DEV
+	cp -v grub.cfg ${MNT}/boot/grub/
 fi
 
-echo "building $LINUX ..."
-if [ ! -f "${ROOTFS}/boot/vmlinuz" ]; then
-	sudo cp /boot/vmlinuz-`uname -r` ${ROOTFS}/boot/vmlinuz -v
-	sudo cp /boot/initrd.img-`uname -r` ${ROOTFS}/boot/initrd.img -v
-	sudo cp -av --parents /lib/modules/`uname -r` $ROOTFS || exit 1
-else
-	cd $BUILD
-#	tar xf $SOURCE/$LINUX.tar.xz && \
-	cd $LINUX && \
-	cp -v $TOP_DIR/qemu_defconfig arch/x86/configs/ && \
-	make qemu_defconfig && \
-	make -j2 && \
-	sudo cp -v arch/x86/boot/bzImage $ROOTFS/boot/vmlinuz && \
-	sudo make INSTALL_MOD_PATH=$ROOTFS modules_install || exit 1
-fi
+sync && umount $MNT
 
-sudo mkdir -vp ${ROOTFS}/{usr,lib,home,proc,sys,dev,etc,tmp}
-
-if [ ! -e "${ROOTFS}/lib/libc.so" ]; then
-	sudo cp -av --parents `find /lib /lib64 -name "*.so*"` $ROOTFS || exit 1
-	# fixme
-	sudo cp -av --parents `ls /usr/lib/$MACH-linux-gnu/*w.so*` $ROOTFS || exit 1
-	sudo cp -av --parents `ls /usr/lib/$MACH-linux-gnu/libltdl.so*` ${ROOTFS}
-fi
-
-if [ ! -e "${ROOTFS}/sbin/init" ]; then
-	cd $BUILD
-	tar xf $SOURCE/${BUSYBOX}.tar.bz2
-	cd ${BUSYBOX}
-	make defconfig > /dev/null
-	make
-	sudo make CONFIG_PREFIX=$ROOTFS install
-fi
-
-if [ ! -e "${ROOTFS}/etc/init.d/rcS" ]; then
-	sudo mkdir -p ${ROOTFS}/etc/init.d/
-	sudo cp -av $TOP_DIR/rcS ${ROOTFS}/etc/init.d/
-	sudo touch ${ROOTFS}/etc/group
-fi
-
-#sudo cp -av --parents `find /usr/lib -name "libasound*.so*"` $ROOTFS/
-#sudo cp -av --parents /usr/share/alsa* $ROOTFS/
-
-cd $BUILD
-tar xf $SOURCE/${ALSA_LIB}.tar.bz2
-cd ${ALSA_LIB}
-./configure --prefix=/usr --disable-python
-make
-sudo make DESTDIR=$ROOTFS install
-
-sudo cp -av --parents `which aplay` $ROOTFS/
-
-cd $BUILD
-tar xf $SOURCE/$MPG123.tar.bz2
-cd $MPG123
-./configure --prefix=/usr --with-audio=alsa && \
-make && \
-sudo make DESTDIR=$ROOTFS install || exit 1
-
-sudo cp -v /maxwit/document/appdevel/src/audio/mixer/mixer ${ROOTFS}
-sudo cp -v /usr/share/sounds/alsa/Front_Right.wav ${ROOTFS}
-
-sync && sudo umount /mnt/2
-
-if [ $ISIMG = 1 ]; then
-	sudo losetup -d $DEV $DEV2
-	qemu-system-$MACH $IMG -soundhw ac97
-fi
+#if [ $ISIMG = 1 ]; then
+#	sudo losetup -d $DEV $DEV2
+#	export QEMU_AUDIO_DRV=alsa
+#	qemu-system-$MACH $IMG -soundhw ac97
+#fi
